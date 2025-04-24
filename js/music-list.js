50f18d96b051c2a5761de197209c546db27e6f16
Itz_Gabby <oshowobigabriel02@gmail.com>
2025-04-24 21:32:02 +0100

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,140 @@
+let allMusic = [ 
+    {
+        name: "Fido - Awolowo",
+        artist: "Fido",
+        img: "Awolowo",
+        src: "Awolowo"
+    },
+    {
+        name: "Asake - Fuji Vibes",
+        artist: "Asake",
+        img: "Asake",
+        src: "Asake"
+    },
+    {
+        name: "Tope Alabi - You Are Worthy ",
+        artist: "Tope Alabi",
+        img: "Tope",
+        src: "Tope"
+    },
+    {
+        name: "Fido - Joy Is Coming",
+        artist: "Fido",
+        img: "Fido",
+        src: "Fido"
+    },
+    {
+        name: "Asake & Wizkid - MMS",
+        artist: "Asake & Wizkid",
+        img: "Asake",
+        src: "As"
+    },
+    {
+        name: "Sola Allyson - Imisi ",
+        artist: "Sola Allyson",
+        img: "Sola",
+        src: "Sola"
+    },
+    {
+        name: "Rema - Baby(Is It A Crime) ",
+        artist: "Rema",
+        img: "Baby",
+        src: "Rema"
+    },
+    {
+        name: "Chella - My Darling ",
+        artist: "Chella",
+        img: "Chella",
+        src: "Chella"
+    },
+    {
+        name: "Vicoka - Port Harcourt ",
+        artist: "Vicoka",
+        img: "Port",
+        src: "Port"
+    },
+    {
+        name: "Lanre - Folasade ",
+        artist: "Lanre",
+        img: "3",
+        src: "Lanre"
+    },
+    {
+        name: "Asake - Uhh Yeah ",
+        artist: "Asake",
+        img: "Asake",
+        src: "Uhh"
+    },
+    {
+        name: "Shallipopi - Laho",
+        artist: "Shallipopi",
+        img: "Laho",
+        src: "Laho"
+    },
+    {
+        name: "Boi chase - Focus ",
+        artist: "Boi Chase",
+        img: "Focus",
+        src: "Focus"
+    },
+    {
+        name: "Asake - Ligali ",
+        artist: "Asake",
+        img: "Asake",
+        src: "Ligali"
+    },
+    {
+        name: "Joblaq - Shishi ",
+        artist: "Joblaq",
+        img: "Shishi",
+        src: "Shishi"
+    },
+    {
+        name: "Seyi Vibez - Shaolin",
+        artist: "Seyi Vibez",
+        img: "Shaolin",
+        src: "Shaolin"
+    },
+    {
+        name: "Buju & Rema - Fi Kan We kan ",
+        artist: "Buju & Rema",
+        img: "Bnxn",
+        src: "Bnxn"
+    },
+    {
+        name: "Kunmie - Arike",
+        artist: "Kunmie",
+        img: "Arike",
+        src: "Arike"
+    },
+    {
+        name: "Ayo Maff - Are you there? ",
+        artist: "Ayo Maff",
+        img: "Ayo",
+        src: "Ayo"
+    },
+    {
+        name: "Asake - Worldwide ",
+        artist: "Asake",
+        img: "Asake",
+        src: "Lungu"
+    },
+    {
+        name: "Teni - Money ",
+        artist: "Teni",
+        img: "TENI",
+        src: "TENI"
+    },
+    {
+        name: "Joeblaq & Zinolesky - Jo Vibe ",
+        artist: "Joeblaq & Zinolesky",
+        img: "Joe",
+        src: "Joe"
+    },
+    {
+        name: "Asake - Why Love ",
+        artist: "Asake",
+        img: "Why",
+        src: "Why"
+    },
+]
